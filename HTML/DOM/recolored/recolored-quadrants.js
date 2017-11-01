737df89517bf5251d563404fdb9ac59127a3b3a3
Elon Jefferson <elekson9@yahoo.com>
2017-10-31 23:18:26 -0400

    completed

--- HTML/DOM/recolored/recolored-quadrants.js
+++ HTML/DOM/recolored/recolored-quadrants.js
@@ -1,1 +1,5 @@
-document
+input = prompt('Choose one: "topLeft", "topRight", "bottomLeft", "bottomRight" \nChoose a valid css color \ne.x "topLeft teal" or "bottomRight #005040"').split(" ")
+
+squares = document.querySelectorAll("div");
+
+document.getElementById(input[0]).style.backgroundColor = input[1]
