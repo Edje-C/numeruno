6e167100f18f511a612f90ca37a3459a3931a1d2
Elon Jefferson <elekson9@yahoo.com>
2017-11-01 00:16:37 -0400

    incomplete, I'm tired

--- HTML/DOM/clicker/clicker.js
+++ HTML/DOM/clicker/clicker.js
@@ -1,1 +1,8 @@
-document
+body = document.getElementsByTagName("body")[0];
+
+w = Number(window.innerWidth);
+
+document.addEventListener("click", function(){
+    alert(w)
+    window.body.style.backgroundColor = "red";
+});
