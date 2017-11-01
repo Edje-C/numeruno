9c81bcc0f4099dc23deff6b01b25bc59ad960cd2
Elon Jefferson <elekson9@yahoo.com>
2017-11-01 10:27:34 -0400

    completed

--- HTML/DOM/clicker/clicker.js
+++ HTML/DOM/clicker/clicker.js
@@ -3,6 +3,9 @@ body = document.getElementsByTagName("body")[0];
 w = Number(window.innerWidth);
 
 document.addEventListener("click", function(){
-    alert(w)
-    window.body.style.backgroundColor = "red";
+    if(event.screenX > w/2){
+        body.style.backgroundColor = "peachpuff";
+    }else{
+        body.style.backgroundColor = "mediumaquamarine";
+    }
 });
