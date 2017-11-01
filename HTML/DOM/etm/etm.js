0a9f254e520b5d9bfda8ac566f510c1149f6717b
Elon Jefferson <elekson9@yahoo.com>
2017-10-31 22:19:43 -0400

    completed

--- HTML/DOM/etm/etm.js
+++ HTML/DOM/etm/etm.js
@@ -1,1 +1,9 @@
-document
+ps = document.querySelectorAll("p")
+
+for(var i=0; i<ps.length; i++){
+    if(i%2===0){
+        ps[i].classList.add("bgGreen")
+    }else{
+        ps[i].classList.add("bgYellow")
+    }
+}
