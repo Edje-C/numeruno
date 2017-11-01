d4d3c65c5125217530ed235efa9dafcd1dc1cdcd
Elon Jefferson <elekson9@yahoo.com>
2017-11-01 00:39:57 -0400

    complete I guess

--- HTML/DOM/echo-er/echo-er.js
+++ HTML/DOM/echo-er/echo-er.js
@@ -1,1 +1,12 @@
-document
+h1 = document.getElementsByTagName("h1")[0];
+punc = ".!?"
+
+document.addEventListener("keypress", function(e){
+    if(e.key === e.key.toUpperCase()){
+        h1.classList.add("upper")
+    }
+    if(punc.includes(e.key)){
+        h1.classList.remove("upper")
+    }
+    h1.innerText += e.key
+})
