833a69876f39396c743008bba7900b6cbf82ed3f
Elon Jefferson <elekson9@yahoo.com>
2017-11-13 16:35:21 -0500

    Added missing quoatation mark

--- HTML/ES6/makestory.js
+++ HTML/ES6/makestory.js
@@ -1,3 +1,3 @@
-const makestory = (name) => `Remember, go straight to Grandma's house," ${name}'s mother cautioned.  "Don't dawdle along the way and please don't talk to strangers!  The woods are dangerous."
+const makestory = (name) => `"Remember, go straight to Grandma's house," ${name}'s mother cautioned.  "Don't dawdle along the way and please don't talk to strangers!  The woods are dangerous."
 
 "Don't worry, mommy," said ${name}, "I'll be careful.`
